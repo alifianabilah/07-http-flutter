91588a5fb26d0dabb8b774e0a13bb458390c2a03
Alifia Nabilah <alifianabilahh@gmail.com>
2022-04-28 13:18:33 +0700

    http request

---
+++ lib/models/movie.dart
@@ -0,0 +1,18 @@
+class Movie {
+  //Alifia Shofa' Nabilah (2031710027)
+  late int id;
+  late String title;
+  late double voteAverage;
+  late String overview;
+  late String posterPath;
+
+  Movie(this.id, this.title, this.voteAverage, this.overview, this.posterPath);
+
+  Movie.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    title = parsedJson['title'];
+    voteAverage = parsedJson['vote_average'] * 1.0;
+    overview = parsedJson['overview'];
+    posterPath = parsedJson['poster_path'];
+  }
+}
---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import '../service/http_service.dart';
+import 'movie_detail.dart';
+
+//Alifia Shofa' Nabilah (2031710027)
+
+class MovieList extends StatefulWidget {
+  const MovieList({Key? key}) : super(key: key);
+
+  @override
+  State<MovieList> createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  late int movieCount = 0;
+  late List movies;
+  late HttpService service;
+  final String imgPath = 'https://image.tmdb.org/t/p/w500/';
+
+  Future initialize() async {
+    movies = [];
+    movies = (await service.getPopularMovies())!;
+    setState(() {
+      movieCount = movies.length;
+      movies = movies;
+    });
+  }
+
+  @override
+  void initState() {
+    service = HttpService();
+    initialize();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Column(
+          children: [
+            Text('Popular Movie'),
+            SizedBox(height: 5),
+            Text('Alifia Shofa Nabilah (2031710027)'),
+          ],
+        )),
+      ),
+      body: ListView.builder(
+        // ignore: unnecessary_null_comparison
+        itemCount: (movieCount == null) ? 0 : movieCount,
+        itemBuilder: (context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                backgroundImage:
+                    NetworkImage(imgPath + movies[position].posterPath),
+              ),
+              title: Text(movies[position].title),
+              subtitle: Text(
+                'Rating = ' + movies[position].voteAverage.toString(),
+              ),
+              onTap: () {
+                MaterialPageRoute route = MaterialPageRoute(
+                    builder: (_) => MovieDetail(movies[position]));
+                Navigator.push(context, route);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
